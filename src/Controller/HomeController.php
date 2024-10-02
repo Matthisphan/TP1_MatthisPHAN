c79d0e8f02338586dad9f2c7cb9e5c82c204736f
Matthis <matthisd77@gmail.com>
2024-10-02 15:14:28 +0200

    Modify: Replace Controller Folder

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,20 +1,15 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Controller;
 
-use App\Entity\Subscription;
-use App\Entity\User;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    #[Route(path: '/', name: 'page_hello')]
-    public function accueil()
-    {
+    #[Route(path: '', name: 'page_home')]
+    public function home(){
         return $this->render('index.html.twig');
     }
 }
