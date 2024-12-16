b5a968cebb931146716a712e16d8c5e52a28c4b9
Matthis <matthisd77@gmail.com>
2024-12-16 14:59:10 +0100

    UPDATE : homepage tendance

--- src/Repository/MediaRepository.php
+++ src/Repository/MediaRepository.php
@@ -15,29 +15,14 @@ class MediaRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Media::class);
     }
-
-//    /**
-//     * @return Media[] Returns an array of Media objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('m')
-//            ->andWhere('m.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('m.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Media
-//    {
-//        return $this->createQueryBuilder('m')
-//            ->andWhere('m.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function findPopular(int $maxResults): array
+    {
+        return $this->createQueryBuilder('m')
+            ->leftJoin('m.watchHistories', 'wh')
+            ->groupBy('m.id')
+            ->orderBy('COUNT(wh)', 'DESC')
+            ->setMaxResults($maxResults)
+            ->getQuery()
+            ->getResult();
+    }
 }
